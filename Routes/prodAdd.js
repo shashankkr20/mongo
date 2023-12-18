bc5c91b59d243a3ef1caf8f443389c10d3d6f52f
pandiji <skm808478@gmail.com>
2023-12-19 02:10:13 +0530

    lmkmk

--- Routes/prodAdd.js
+++ Routes/prodAdd.js
@@ -1,31 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const Product = require('../schema/product');
-
-router.post('/', async (req, res) => {
-  try {
-    const { name } = req.body;
-
-    // Validate required fields
-    if (!name) {
-      return res.status(400).json({ error: 'Missing required fields' });
+const express=require('express')
+const app=express();
+const prods=require('./../schema/product')
+const addprod=app.post('/',async(req,res)=>{
+    try{
+        console.log(req.body.name)
+        const  adduser=await prods.create(req.body).then(res.send("added"))
+    // res.send("added")
     }
-
-    // Check if the product already exists based on unique criteria (e.g., name)
-    const existingProduct = await Product.findOne({ name });
-    if (existingProduct) {
-      return res.status(400).json({ error: 'Product with this name already exists' });
+    catch(err){
+        console.log(err)
+        res.send("error")
     }
-
-    // Create a new product
-    const newProduct = await Product.create(req.body);
-
-    // Send success response
-    res.status(201).json({ message: 'Product added successfully', product: newProduct });
-  } catch (err) {
-    console.error('Error adding product:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-module.exports = router;
+    
+})
+module.exports=addprod
