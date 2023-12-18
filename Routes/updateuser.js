bd8239c518c2a35833d5c86556e2cf2502ebf3c3
pandiji <skm808478@gmail.com>
2023-12-19 01:53:02 +0530

    kjkjk

--- Routes/updateuser.js
+++ Routes/updateuser.js
@@ -1,41 +1,20 @@
-const express = require('express');
-const app = express();
-const users = require('./../schema/user');
-
-const upduser = async (req, res, next) => {
-  try {
-    console.log("User Update Search");
-    console.log(req.params.id);
-
-    // Ensure that req.params.id is a valid MongoDB ObjectId string
-    if (!/^[0-9a-fA-F]{24}$/.test(req.params.id)) {
-      return res.status(400).send('Invalid ObjectId format');
+const express=require('express')
+const app=express();
+const users=require('./../schema/user')
+const upduser=app.get('/:id',async(req,res)=>{
+    try{
+        console.log(req.params.id)
+        const adduser=await users.findOne({"_id":req.params.id})
+        res.send(adduser)
+           
+        
+        
+        
     }
-
-    const user = await users.findOne({ "_id": req.params.id });
-
-    // Check if the user was found
-    if (!user) {
-      return res.status(404).send('User not found for the provided ID');
+    catch(err){
+        console.log(err)
+        res.send("error")
     }
+})
 
-    res.send(user);
-  } catch (err) {
-    console.error(err);
-    next(err); // Pass the error to the next middleware
-  }
-};
-
-// Global error handling middleware
-const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('Internal Server Error');
-};
-
-// Use the upduser function for the route
-app.get('/:id', upduser);
-
-// Use the error handling middleware
-app.use(errorHandler);
-
-module.exports = app;
+module.exports=upduser
